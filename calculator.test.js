af86428c66729765a949945fed640d5348cc64e4
Nick <nickmagi24@gmail.com>
2021-10-18 16:39:59 -0400

    Calculator test pass

--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,17 @@
 const calculator = require('./calculator');
 
 test('Adds integers', () => {
-    expect(add(2, 2)).toBe(4);
+    expect(calculator.add(2, 2)).toBe(4);
 });
 
 test('subtracts integers', () => {
-    expect(subtract(7, 3)).toBe(4);
+    expect(calculator.subtract(7, 3)).toBe(4);
 });
 
 test('Multiples integers', () => {
-    expect(multiply(5, 5)).toBe(25);
+    expect(calculator.multiply(5, 5)).toBe(25);
 });
 
 test('Divides integers', () => {
-    expect(divide(5, 10)).toBe(2);
+    expect(calculator.divide(10, 5)).toBe(2);
 });
