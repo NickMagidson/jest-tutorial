7fd5278003f4d795d340159938a53e02836cb4ea
Nick <nickmagi24@gmail.com>
2021-10-19 13:07:00 -0400

    Ceasar Cipher code

--- ceasarCipher.js
+++ ceasarCipher.js
@@ -1,6 +1,9 @@
 function ceasarCipher(string, shift) {
-    //
+    if (shift === 0) return string;
+    
+    return string.replace(/[a-z]/g, (char) => 
+    String.fromCharCode(((char.charCodeAt(0) - 97 + shift) % 26) + 97)
+    );
+};
 
-    //For loop (i < string.length)
-    //
-}
+module.exports = ceasarCipher;
